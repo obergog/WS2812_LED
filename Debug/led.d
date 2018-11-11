b30ec4d0c6fa5ff330e7d7efca0a101a77d33e14
obergog <alex.m.johnson@colorado.edu>
2018-11-11 12:32:05 -0700

    Added CCS project files

---
+++ Debug/led.d
@@ -0,0 +1,37 @@
+# FIXED
+
+led.obj: ../led.c
+led.obj: /home/alex/ti/ccsv8/ccs_base/arm/include/msp.h
+led.obj: /home/alex/ti/ccsv8/ccs_base/arm/include/msp432p401r.h
+led.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h
+led.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h
+led.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h
+led.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h
+led.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h
+led.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h
+led.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h
+led.obj: /home/alex/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h
+led.obj: /home/alex/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h
+led.obj: /home/alex/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h
+led.obj: /home/alex/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+led.obj: /home/alex/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+led.obj: /home/alex/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h
+led.obj: ../led.h
+
+../led.c: 
+/home/alex/ti/ccsv8/ccs_base/arm/include/msp.h: 
+/home/alex/ti/ccsv8/ccs_base/arm/include/msp432p401r.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h: 
+/home/alex/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h: 
+/home/alex/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h: 
+/home/alex/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h: 
+/home/alex/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+/home/alex/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+/home/alex/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h: 
+../led.h: 
---
+++ Debug/startup_msp432p401r_ccs.d
@@ -0,0 +1,19 @@
+# FIXED
+
+startup_msp432p401r_ccs.obj: ../startup_msp432p401r_ccs.c
+startup_msp432p401r_ccs.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h
+startup_msp432p401r_ccs.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h
+startup_msp432p401r_ccs.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h
+startup_msp432p401r_ccs.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h
+startup_msp432p401r_ccs.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h
+startup_msp432p401r_ccs.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h
+startup_msp432p401r_ccs.obj: /home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h
+
+../startup_msp432p401r_ccs.c: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h: 
+/home/alex/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h: 
